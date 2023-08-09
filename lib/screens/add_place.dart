699cd152eae02053f7d3d760bb8fcc5b768bbc62
mlnbk <dev.milan.bako@gmail.com>
2023-08-09 10:55:42 +0200

    feat: allow taking pictures
    * add pick an image input
    * add `image_picker` package
    * use device camera to take pictures

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -68,6 +69,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               style:
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
+            const SizedBox(height: 16),
+            ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
